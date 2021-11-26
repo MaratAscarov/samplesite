67cc962c1e97993f44a1b3ff2bf650674938cbeb
MaratAscarov <marat011383@yandex.ru>
2021-11-26 19:56:54 +0300

    edit views.py

--- bboard/views.py
+++ bboard/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponce("Здесь будет выведен список объявлений.")
+    return HttpResponse("Здесь будет выведен список объявлений.")
