83e1fe9724ea1205b053e86f05d77c92c40cf00b
MaratAscarov <marat011383@yandex.ru>
2022-04-06 00:19:28 +0300

    Edit bboard\views.py

--- bboard/views.py
+++ bboard/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Bb
 
 # Create your views here.
 
 def index(request):
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    '''
     s = 'Список объявлений \r\n\r\n\r\n'
     for bb in Bb.objects.order_by('-published'):
         s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-
-    return HttpResponse(s, content_type = 'text/plain; charset=utf-8')
+    '''
+    return HttpResponse(template.render(context, request))     
